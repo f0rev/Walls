b4e81ef764bf4eb15f432394f9df21a0a2c517ac
ravan <ravan.latifli@yahoo.com>
2018-08-04 00:55:18 +0300

    second part update

--- app/src/main/java/com/feevx/ravan/worldcupwallpapers/ui/photodetails/FullScreenImageAdapter.java
+++ app/src/main/java/com/feevx/ravan/worldcupwallpapers/ui/photodetails/FullScreenImageAdapter.java
@@ -2,15 +2,21 @@ package com.feevx.ravan.worldcupwallpapers.ui.photodetails;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.feevx.ravan.worldcupwallpapers.R;
 import com.feevx.ravan.worldcupwallpapers.configs.GlideApp;
 import com.feevx.ravan.worldcupwallpapers.configs.widgets.TouchImageView;
@@ -27,10 +33,21 @@ public class FullScreenImageAdapter extends PagerAdapter {
     private Activity mActivity;
     private List<Photo> mPhotos = new ArrayList<>();
     private LayoutInflater inflater;
+    private SetWallpaperClick mSetWallpaperClick;
+    private BackClick mBackClick;
 
-    // constructor
-    public FullScreenImageAdapter(Activity activity) {
+    public interface SetWallpaperClick {
+        void onSetClicked(Photo photo);
+    }
+
+    public interface BackClick {
+        void onBackPressed();
+    }
+
+    public FullScreenImageAdapter(Activity activity, SetWallpaperClick setWallpaperClick, BackClick backClick) {
         mActivity = activity;
+        mSetWallpaperClick = setWallpaperClick;
+        mBackClick = backClick;
     }
 
     @Override
@@ -40,44 +57,61 @@ public class FullScreenImageAdapter extends PagerAdapter {
 
     @Override
     public boolean isViewFromObject(View view, Object object) {
-        return view == ((RelativeLayout) object);
+        return view == (object);
     }
 
     @Override
     public Object instantiateItem(ViewGroup container, int position) {
         TouchImageView imgDisplay;
-        Button btnClose;
+        Button setWallpaperBtn;
+        Button backBtn;
+        ProgressBar loading;
 
         inflater = (LayoutInflater) mActivity
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View viewLayout = inflater.inflate(R.layout.layout_fullscreen_image, container,
                 false);
 
-        imgDisplay =  viewLayout.findViewById(R.id.imgDisplay);
-        btnClose =  viewLayout.findViewById(R.id.btnClose);
+        imgDisplay = viewLayout.findViewById(R.id.imgDisplay);
+        setWallpaperBtn = viewLayout.findViewById(R.id.set_wallpaper);
+        loading = viewLayout.findViewById(R.id.loading);
+        backBtn = viewLayout.findViewById(R.id.back);
 
-        GlideApp.with(mActivity).load(mPhotos.get(position).getUrls().getFull()).into(imgDisplay);
+        GlideApp.with(mActivity).load(mPhotos.get(position).getUrls().getFull()).listener(new RequestListener<Drawable>() {
+            @Override
+            public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                loading.setVisibility(View.GONE);
+                return false;
+            }
 
-        // close button click event
-//        btnClose.setOnClickListener(v -> mActivity.finish());
+            @Override
+            public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                loading.setVisibility(View.GONE);
+                return false;
+            }
+        }).into(imgDisplay);
 
-        ((ViewPager) container).addView(viewLayout);
+        setWallpaperBtn.setOnClickListener(v -> mSetWallpaperClick.onSetClicked(mPhotos.get(position)));
+        backBtn.setOnClickListener(v -> mBackClick.onBackPressed());
+
+        (container).addView(viewLayout);
 
         return viewLayout;
     }
 
     @Override
     public void destroyItem(ViewGroup container, int position, Object object) {
-        ((ViewPager) container).removeView((RelativeLayout) object);
+        (container).removeView((RelativeLayout) object);
 
     }
 
-    public void updateData(List<Photo> data){
+    public void updateData(List<Photo> data) {
         mPhotos.clear();
         mPhotos.addAll(data);
         notifyDataSetChanged();
     }
-    public void addData(List<Photo> data){
+
+    public void addData(List<Photo> data) {
         mPhotos.addAll(data);
         notifyDataSetChanged();
     }
--- app/src/main/java/com/feevx/ravan/worldcupwallpapers/ui/photodetails/PhotoDetailsFragment.java
+++ app/src/main/java/com/feevx/ravan/worldcupwallpapers/ui/photodetails/PhotoDetailsFragment.java
@@ -1,55 +1,63 @@
 package com.feevx.ravan.worldcupwallpapers.ui.photodetails;
 
 
+import android.app.ProgressDialog;
+import android.app.WallpaperManager;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
-import android.os.Build;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 
 import com.feevx.ravan.worldcupwallpapers.R;
+import com.feevx.ravan.worldcupwallpapers.configs.GlideApp;
+import com.feevx.ravan.worldcupwallpapers.configs.GlideRequest;
 import com.feevx.ravan.worldcupwallpapers.ui.MainActivity;
 import com.feevx.ravan.worldcupwallpapers.viewmodels.PhotosViewModel;
+import com.kc.unsplash.models.Photo;
+
+import java.io.IOException;
 
 import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
+import dagger.android.support.AndroidSupportInjection;
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.schedulers.Schedulers;
+import io.reactivex.subjects.PublishSubject;
+import timber.log.Timber;
 
 /**
  * ‍ Spelled 💫  by 🧙 maester ravan on 02/08/2018 in Arcane Castle ✨🏰✨
  **/
 
-public class PhotoDetailsFragment extends Fragment {
+public class PhotoDetailsFragment extends Fragment implements FullScreenImageAdapter.SetWallpaperClick, FullScreenImageAdapter.BackClick {
 
     @Inject
     Context mContext;
 
     private static final String PHOTO_INDEX = "photoIndex";
+
     @BindView(R.id.pager)
     ViewPager mPager;
+
     Unbinder unbinder;
     private int mPhotoIndex;
     private PhotosViewModel mPhotoViewModel;
     private FullScreenImageAdapter mFullScreenImageAdapter;
     private MainActivity mMainActivity;
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        mMainActivity = (MainActivity) getActivity();
-
-
-    }
+    private Bitmap readyBitmap;
+    private CompositeDisposable mCompositeDisposable;
+    private ProgressDialog mProgressDialog;
 
     public static PhotoDetailsFragment newInstance(int photoIndex) {
         PhotoDetailsFragment albumDetailsFragment = new PhotoDetailsFragment();
@@ -65,42 +73,33 @@ public class PhotoDetailsFragment extends Fragment {
     }
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Bundle args = getArguments();
-        if (args != null)
-            mPhotoIndex = args.getInt(PHOTO_INDEX);
-        configureViewmodel();
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mMainActivity = (MainActivity) getActivity();
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_photo_details, container, false);
         unbinder = ButterKnife.bind(this, view);
-
         return view;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        Bundle args = getArguments();
+        if (args != null)
+            mPhotoIndex = args.getInt(PHOTO_INDEX);
+
+        mCompositeDisposable = new CompositeDisposable();
+        configureViewmodel();
+        configureDagger();
         mMainActivity.fullScreen(true);
-        mFullScreenImageAdapter = new FullScreenImageAdapter(getActivity());
+        mFullScreenImageAdapter = new FullScreenImageAdapter(getActivity(), this, this);
         mPager.setAdapter(mFullScreenImageAdapter);
-        mPager.setCurrentItem(mPhotoIndex);
-
-
-    }
-
-    private void configureViewmodel() {
-        mPhotoViewModel = ViewModelProviders.of(mMainActivity).get(PhotosViewModel.class);
-        mPhotoViewModel.getLivePhotoData().observe(this, data -> {
-            if (data.size() > 20)
-                mFullScreenImageAdapter.addData(data);
-            else
-                mFullScreenImageAdapter.updateData(data);
-        });
     }
 
     @Override
@@ -112,6 +111,73 @@ public class PhotoDetailsFragment extends Fragment {
     @Override
     public void onDestroy() {
         super.onDestroy();
+        mCompositeDisposable.dispose();
         mMainActivity.fullScreen(false);
     }
+
+    @Override
+    public void onSetClicked(Photo photo) {
+        mProgressDialog = ProgressDialog.show(mMainActivity, null,
+                "Обой устанавливается", true);
+
+        PublishSubject<Bitmap> stop = PublishSubject.create();
+        Observable<Bitmap> bitmapObservable = getBitmapFromUrl(photo);
+        mCompositeDisposable.add(bitmapObservable
+                .takeUntil(stop)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnComplete(() -> {
+                    setBitmapAsWallpaper(readyBitmap);
+                })
+                .subscribe(bitmap -> {
+                    readyBitmap = bitmap;
+                    stop.onComplete();
+                }, Timber::e));
+    }
+
+
+    public Observable<Bitmap> getBitmapFromUrl(final Photo photo) {
+        GlideRequest<Bitmap> glideRequest = GlideApp.with(mContext)
+                .asBitmap();
+        return Observable.create(emitter -> {
+            Bitmap bitmap = glideRequest.load(photo.getUrls().getFull())
+                    .submit()
+                    .get();
+            emitter.onNext(bitmap);
+        });
+    }
+
+    void setBitmapAsWallpaper(Bitmap bitmap) {
+        WallpaperManager wallpaperManager = WallpaperManager.getInstance(mContext);
+        try {
+            wallpaperManager.setBitmap(bitmap);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            mProgressDialog.dismiss();
+        }
+    }
+
+    private void configureDagger() {
+        AndroidSupportInjection.inject(this);
+    }
+
+    private void configureViewmodel() {
+        mPhotoViewModel = ViewModelProviders.of(mMainActivity).get(PhotosViewModel.class);
+        mPhotoViewModel.getLivePhotoData().observe(this, data -> {
+            if (data.size() > 20)
+                mFullScreenImageAdapter.addData(data);
+            else {
+                mFullScreenImageAdapter.updateData(data);
+                mPager.setCurrentItem(mPhotoIndex);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        assert getFragmentManager() != null;
+        getFragmentManager().popBackStack();
+    }
 }
+
